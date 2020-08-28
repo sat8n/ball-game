b25e72c3f08420c656d5d97aa4def2d49da2cb50
Nabilah Abd Malek <na17967@essex.ac.uk>
2020-08-28 11:18:13 +0800

    Initialised ServerProgram.java

---
+++ BallGameJava/src/gameServer/ServerProgram.java
@@ -0,0 +1,31 @@
+package gameServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerProgram {
+    private final static int port = 6666;
+    private final static Game game = new Game();
+
+    private static void RunServer() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Waiting for incoming connections...");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket, game)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        game.startGame();
+
+        RunServer();
+    }
+    // end
+}
